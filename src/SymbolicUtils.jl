c914646802c9b3d686fa0d2037729b8506901df5
Shashi Gowda <shashigowda91@gmail.com>
2020-02-12 13:05:48 -0500

    type paramize symtype

--- src/SymbolicUtils.jl
+++ src/SymbolicUtils.jl
@@ -1,8 +1,9 @@
 module SymbolicUtils
 
-abstract type Symbolic end
+abstract type Symbolic{T} end
 
 symtype(x) = typeof(x) # For types outside of SymbolicUtils
+symtype(::Symbolic{T}) where {T} = T
 
 @noinline function promote_symtype(f, xs...)
     error("promote_symtype($f, $(join(xs, ", "))) not defined")

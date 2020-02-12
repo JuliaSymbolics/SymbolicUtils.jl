5ebc14b283397d85155c2a8cfb1e881a2e7a5fee
Shashi Gowda <shashigowda91@gmail.com>
2020-02-11 21:31:00 -0500

    simplification rules flatten and sort arguments
    Co-authored-by: "Yingbo Ma" <mayingbo5@gmail.com>
    Co-authored-by: "Shashi Gowda" <gowda@mit.edu>

--- src/simplify.jl
+++ src/simplify.jl
@@ -8,11 +8,12 @@
 <ₑ(a::Number,   b::Symbolic) = false
 
 <ₑ(a::Variable, b::Symbolic) = true
-<ₑ(a::Variable, b::Variable) = a.name <ₑ b.name
+<ₑ(a::Variable, b::Variable) = a.name < b.name
+<ₑ(a::T, b::S) where {T, S} = T===S ? isless(a, b) : nameof(T) < nameof(S)
 
 function <ₑ(a::Term, b::Term)
-    na = nameof(a)
-    nb = nameof(b)
+    na = nameof(operation(a))
+    nb = nameof(operation(b))
     if na !== nb
         return na < nb
     else
@@ -20,67 +21,74 @@ function <ₑ(a::Term, b::Term)
         if length(aa) !== length(ab)
             return aa < ab
         else
-            return any(<ₑ, aa, ab) || false
+            return any((a<ₑ b for (a, b) in zip(aa, ab))) || false
         end
     end
 end
+
 isnumber(x) = x isa Number
+issortedₑ(args) = issorted(args, lt=<ₑ)
+issortedₑ(args::Tuple) = issorted([args...], lt=<ₑ)
+issortedₑ((a,b,)::Tuple{Any,Any}) = isequal(a, b) || a <ₑ b
+issortedₑ(a::Tuple{Any}) = true
 
-const plus_and_scalar_mul = let
+function isnotflat(⋆)
+    function (args)
+        for t in args
+            if t isa Term && operation(t) === (⋆)
+                return true
+            end
+        end
+        return false
+    end
+end
+
+PLUS_AND_SCALAR_MUL = let
     [
-     # Associativity of * (moves numbers to the end)
-     @rule(*(~~y) => *(flatten_args(*, 1, ~y)...)),
+     #@rule(*(~~x, *(~~y), ~~z) => *((~~x)..., (~~y)..., (~~z)...)),
+     # Flatten *
+     @rule(*(~~x::isnotflat(*)) => flatten_term(*, ~~x)),
+     # Commute *
+     @rule(*(~~x::!(issortedₑ)) => sort_args(*, ~~x)),
 
-     # Associativity of + (also moves numbers to the back)
-     @rule(+(~~x) => +(flatten_args(+, 0, ~x)...)),
+     @rule(~x - ~y => ~x + (-~y)),
+     #@rule(+(~~x, +(~~y), ~~z) => +((~~x)..., (~~y)..., (~~z)...)),
+     # Flatten +
+     @rule(+(~~x::isnotflat(+)) => flatten_term(+, ~~x)),
+     # Commute +
+     @rule(+(~~x::!(issortedₑ)) => sort_args(+, ~~x)),
 
-     # Commutativity of +
-     @rule(+(~~x) => sort_args(~x)),
-
-     # addition of the same term
+     # Group terms
      @rule(+(~~a, *(~~x, ~α::isnumber), *(~~x, ~β::isnumber), ~~b) => +((~~a)..., *(*(~α, ~β), (~x)...), (~b)...))
+
+     # Group terms
     ]
 end
 
-function move_numbers_to_end(args)
-    if !any(isnumber, args)
-        return nothing
-    else
-        flatten_args(*, 1, args)
-    end
-end
+simplifynum(x) = rewriter(PLUS_AND_SCALAR_MUL)(x)
 
 # Numbers to the back
-function flatten_args(⋆, init, args)
-    # flatten nested +
-    flattened_numeric  = Number[]
-    flattened_symbolic = Any[]
-
+function flatten_term(⋆, args)
+    # flatten nested ⋆
+    flattened_args = []
     for t in args
         if t isa Term && operation(t) === (⋆)
-            for a in arguments(t)
-                if t isa Number
-                    push!(flattened_numeric, a)
-                else
-                    push!(flattened_symbolic, a)
-                end
-            end
+            append!(flattened_args, arguments(t))
+        else
+            push!(flattened_args, t)
         end
     end
-    s = reduce(⋆, flattened_numeric, init=init)
-    s == init ? flattened_symbolic : push!(flattened_symbolic, s)
+    Term(⋆, Number, flattened_args)
 end
 
-function sort_args(args)
+function sort_args(f, args)
     if length(args) < 2
-        return args
+        return Term(f, Number, args)
     elseif length(args) == 2
         x, y = args
-        return x <ₑ y ? nothing : (y,x)
+        return Term(f, Number, x <ₑ y ? [x,y] : [y,x])
     end
 
-    if issorted(args, lt=<ₑ)
-        return nothing
-    end
-    sort!(args, lt=<ₑ)
+    args = args isa Tuple ? [args...] : args
+    Term(f, Number, sort(args, lt=<ₑ))
 end

0d2b2076e321b7e254b44fc9121ad550c57bdcad
MasonProtter <mason.protter@icloud.com>
2020-04-26 13:30:44 -0600

    update interface tests

--- test/interface.jl
+++ test/interface.jl
@@ -1,22 +1,24 @@
+using SymbolicUtils, Test
+using SymbolicUtils: Term, to_symbolic, istree, operation, arguments
 
-using SymbolicUtils: Term, to_symbolic
-SymbolicUtils.symtype(::Union{Expr, Symbol}) = Number
 SymbolicUtils.istree(ex::Expr) = ex.head == :call
 SymbolicUtils.operation(ex::Expr) = ex.args[1]
 SymbolicUtils.arguments(ex::Expr) = ex.args[2:end]
 
 for f ∈ [:+, :-, :*, :/, :^]
     @eval begin
-        Base.$f(x::Union{Expr, Symbol}, y::Number) = Expr(:call, $f, x, y) |> to_symbolic
-        Base.$f(x::Number, y::Union{Expr, Symbol}) = Expr(:call, $f, x, y) |> to_symbolic
-        Base.$f(x::Union{Expr, Symbol}, y::Union{Expr, Symbol}) = (Expr(:call, $f, x, y)) |> to_symbolic
+        Base.$f(x::Union{Expr, Symbol}, y::Number) = Expr(:call, $f, x, y)
+        Base.$f(x::Number, y::Union{Expr, Symbol}) = Expr(:call, $f, x, y)
+        Base.$f(x::Union{Expr, Symbol}, y::Union{Expr, Symbol}) = (Expr(:call, $f, x, y))
     end
 end
-SymbolicUtils.nameof(f::Symbol) = f
-
 
 ex = 1 + (:x - 2)
 
-@test to_symbolic(ex) == Term{Number}(+, [1, Term{Number}(-, [:x, 2])])
-@test simplify(ex) == -1 + :x
+@test to_symbolic(ex) == Term{Any}(+, [1, Term{Any}(-, [:x, 2])])
+@test simplify(ex) == to_symbolic(-1 + :x)
 
+to_expr(t::Term) = Expr(:call, operation(t), to_expr.(arguments(t))...) 
+to_expr(x) = x
+
+to_expr(simplify(ex))

f78275c691841bfd68fd5da080a6219126966e2b
Shashi Gowda <shashigowda91@gmail.com>
2020-04-26 00:41:14 -0400

    Revert "woraround rulesets coverage"
    This reverts commit 512dbffb447e2cdb92c7e724df1e2efa29de01b5.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,6 @@ SymbolicUtils.show_simplified[] = false
 
 #using SymbolicUtils: Rule
 
-# Workaround coverage stats for src/rulesets.jl file.
-@eval SymbolicUtils include(joinpath(@__DIR__, "..", "src", "rulesets.jl"))
-
 include("basics.jl")
 include("rewrite.jl")
 include("rulesets.jl")

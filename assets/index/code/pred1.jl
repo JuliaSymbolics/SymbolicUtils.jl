2b68ec402a9958f9ddc7da95a394b830fe060c9b
shashi <shashigowda91@gmail.com>
2020-04-30 05:08:43 +0000

    Deploying to gh-pages from  @ 8ffcfe2901bc9be050162352e2c1edbfbcc8d80f 🚀

--- assets/index/code/pred1.jl
+++ assets/index/code/pred1.jl
@@ -1,5 +1,6 @@
 # This file was generated, do not modify it. # hide
 r = @rule ~x + ~~y::(ys->iseven(length(ys))) => "odd terms"
 
+@show r(w + z + z + w)
 @show r(w + z + z)
 @show r(w + z)
---
+++ assets/index/code/symtype.jl
@@ -0,0 +1,4 @@
+# This file was generated, do not modify it. # hide
+using SymbolicUtils: symtype
+
+symtype(w), symtype(z),  symtype(α), symtype(β)

af55cae20d0afd77506ca578f84d8d2fabf7c0e4
Shashi Gowda <shashigowda91@gmail.com>
2020-05-02 15:25:41 -0400

    benchmark tweaks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,13 +6,15 @@ SUITE = BenchmarkGroup()
 
 @syms a b c
 let r = @rule(~x => ~x), rs = RuleSet([r])
-    rewriter_suite = SUITE["rewriter"]  = BenchmarkGroup()
+    overhead = SUITE["overhead"]  = BenchmarkGroup()
+    overhead["rule"]  = BenchmarkGroup()
 
-    rewriter_suite["rule:noop:Int"]  = @benchmarkable $r(1)
-    rewriter_suite["rule:noop:Sym"]  = @benchmarkable $r(a)
-    rewriter_suite["rule:noop:Term"] = @benchmarkable $r(a+2)
+    overhead["rule"]["noop:Int"]  = @benchmarkable $r(1)
+    overhead["rule"]["noop:Sym"]  = @benchmarkable $r($a)
+    overhead["rule"]["noop:Term"] = @benchmarkable $r($(a+2))
 
-    rewriter_suite["ruleset:noop:Int"]  = @benchmarkable $rs(1)
-    rewriter_suite["ruleset:noop:Sym"]  = @benchmarkable $rs(a)
-    rewriter_suite["ruleset:noop:Term"] = @benchmarkable $rs(a+2)
+    overhead["ruleset"]  = BenchmarkGroup()
+    overhead["ruleset"]["noop:Int"]  = @benchmarkable $rs(1)
+    overhead["ruleset"]["noop:Sym"]  = @benchmarkable $rs($a)
+    overhead["ruleset"]["noop:Term"] = @benchmarkable $rs($(a+2))
 end

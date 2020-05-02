9327983a0745167e2f34c61d21e003877028b790
Shashi Gowda <shashigowda91@gmail.com>
2020-05-02 16:57:55 -0400

    try to run benchmarks on travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,3 +11,17 @@ include("rewrite.jl")
 include("rulesets.jl")
 include("interface.jl")
 include("fuzz.jl")
+
+if haskey(ENV, "TRAVIS")
+    # A little trick for travis
+    using PkgBenchmark
+
+    pkgpath = dirname(dirname(pathof(SymbolicUtils)))
+    # move it out of the repository so that you can check out different branches
+    script = tempname() * ".jl"
+    benchpath = joinpath(pkgpath, "benchmark", "benchmarks.jl")
+    cp(benchpath, script)
+
+    j = judge(pkgpath, "master", retune=true, script=script)
+    export_markdown(stdout, j, export_invariants=true)
+end

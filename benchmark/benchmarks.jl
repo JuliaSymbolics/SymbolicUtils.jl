8e7fe08a59059535a66c267a47fa064226e9b77b
Shashi Gowda <shashigowda91@gmail.com>
2020-05-03 10:44:28 -0400

    add acrules to benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,13 +5,22 @@ using Random
 SUITE = BenchmarkGroup()
 
 @syms a b c
-let r = @rule(~x => ~x), rs = RuleSet([r])
+let r = @rule(~x => ~x), rs = RuleSet([r]),
+    acr = @rule(~x::isnumber + ~y => ~y)
+
     overhead = SUITE["overhead"]  = BenchmarkGroup()
     overhead["rule"]  = BenchmarkGroup()
 
     overhead["rule"]["noop:Int"]  = @benchmarkable $r(1)
     overhead["rule"]["noop:Sym"]  = @benchmarkable $r($a)
     overhead["rule"]["noop:Term"] = @benchmarkable $r($(a+2))
+
+    overhead["acrule"]  = BenchmarkGroup()
+    overhead["acrule"]["noop:Int"]  = @benchmarkable $r(1)
+    overhead["acrule"]["noop:Sym"]  = @benchmarkable $r($a)
+    overhead["acrule"]["a+2"] = @benchmarkable $r($(a+2))
+    overhead["acrule"]["a+b"] = @benchmarkable $r($(a+b))
+    overhead["acrule"]["a+2+b"] = @benchmarkable $r($(a+2+b))
 
     overhead["ruleset"]  = BenchmarkGroup()
     overhead["ruleset"]["noop:Int"]  = @benchmarkable $rs(1)

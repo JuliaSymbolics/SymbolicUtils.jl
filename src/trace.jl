347656c560b4449aaa6e3eccbe30428cea439080
Shashi Gowda <shashigowda91@gmail.com>
2020-05-10 12:03:58 -0400

    unmike indentation [ci skip]

--- src/trace.jl
+++ src/trace.jl
@@ -9,46 +9,48 @@ collect_names(x, names) = return
 collect_names(x::Symbol, names) = push!(names, (x, :Float64))
 
 function collect_names(x::Expr, names)
-  if isexpr(x, :(::)) && x.args[1] isa Symbol
-    push!(names, (x.args[1], x.args[2]))
-  elseif isexpr(x, :call)
-    foreach(x -> collect_names(x, names), x.args[2:end])
-  else
-    foreach(x -> collect_names(x, names), x.args)
-  end
+    if isexpr(x, :(::)) && x.args[1] isa Symbol
+        push!(names, (x.args[1], x.args[2]))
+    elseif isexpr(x, :call)
+        foreach(x -> collect_names(x, names), x.args[2:end])
+    else
+        foreach(x -> collect_names(x, names), x.args)
+    end
 end
 
 function collect_names(x)
-  names = []
-  collect_names(x, names)
-  return names
+    names = []
+    collect_names(x, names)
+    return names
+end
+
+macro symbolic(ex)
+    names = collect_names(ex)
+    Ts = [T for (x, T) in names]
+    f = :(($(first.(names)...),) -> $ex)
+    quote
+        f = $(esc(f))
+        ir = trace(Mjolnir.Defaults(), Const(f), $(esc.(Ts)...))
+        syms = ($([:(Sym{$(esc(T))}($(QuoteNode(name)))) for (name, T) in names]...),)
+        irterm(ir, syms)
+    end
 end
 
 irterm(ir::IR, v, args) = v
 
 function irterm(ir::IR, v::IRTools.Variable, args)
-  arg = findfirst(==(v), IRTools.arguments(ir))
-  arg == nothing || return args[arg-1]
-  ex = ir[v].expr
-  if isexpr(ex, :call)
-    return term(irterm.((ir,), ex.args, (args,))...)
-  else
-    return ex
-  end
+    arg = findfirst(==(v), IRTools.arguments(ir))
+    arg != nothing && return args[arg-1]
+    ex = ir[v].expr
+    if isexpr(ex, :call)
+        return term(irterm.((ir,), ex.args, (args,))...)
+    else
+        return ex
+    end
 end
 
 irterm(ir::IR, args) = irterm(ir, IRTools.returnvalue(IRTools.blocks(ir)[end]), args)
 
-macro symbolic(ex)
-  names = collect_names(ex)
-  Ts = [T for (x, T) in names]
-  f = :(($(first.(names)...),) -> $ex)
-  quote
-    f = $(esc(f))
-    ir = trace(Mjolnir.Defaults(), Const(f), $(esc.(Ts)...))
-    syms = ($([:(Sym{$(esc(T))}($(QuoteNode(name)))) for (name, T) in names]...),)
-    irterm(ir, syms)
-  end
 function to_mjolnir!(s::Sym, ir, mod, varmap)
     haskey(varmap, s) ? varmap[s] : GlobalRef(mod, nameof(s))
 end

8eb68132cfde17b23abf6b814639a9bd85a34ed7
Shashi Gowda <shashigowda91@gmail.com>
2020-05-12 01:48:16 -0400

    move timeroutputs code to util.jl

--- src/SymbolicUtils.jl
+++ src/SymbolicUtils.jl
@@ -1,32 +1,5 @@
 module SymbolicUtils
 
-const TIMER_OUTPUTS = true
-const being_timed = Ref{Bool}(false)
-
-if TIMER_OUTPUTS
-    using TimerOutputs
-
-    macro timer(name, expr)
-        :(if being_timed[]
-              @timeit $(esc(name)) $(esc(expr))
-          else
-              $(esc(expr))
-          end)
-    end
-
-    macro iftimer(expr)
-        esc(expr)
-    end
-
-else
-    macro timer(name, expr)
-        esc(expr)
-    end
-
-    macro iftimer(expr)
-    end
-end
-
 export @syms, term, @fun, showraw
 include("types.jl")
 
--- src/util.jl
+++ src/util.jl
@@ -8,29 +8,69 @@ struct LL{V}
 end
 
 islist(::Any) = false
+
 islist(l::Union{LL, Term, AbstractArray, Tuple}) = true
 
 Base.empty(l::LL) = empty(l.v)
+
 Base.isempty(l::LL) = l.i > length(l.v)
 
 Base.length(l::LL) = length(l.v)-l.i+1
+
 @inline car(l::LL) = l.v[l.i]
+
 @inline cdr(l::LL) = isempty(l) ? empty(l) : LL(l.v, l.i+1)
 
 Base.length(t::Term) = length(arguments(t)) + 1 # PIRACY
+
 Base.isempty(t::Term) = false
+
 @inline car(t::Term) = operation(t)
+
 @inline cdr(t::Term) = arguments(t)
 
 @inline car(v) = first(v)
+
 @inline cdr(v) = isempty(v) ? empty(l) : LL(v, 2)
 
-@inline take_n(ll::LL, n) = isempty(ll) || n == 0 ? empty(ll) : @views ll.v[ll.i:n+ll.i-1] # @views handles Tuple
+@inline take_n(ll::LL, n) = isempty(ll) || n == 0 ? empty(ll) : @views ll.v[ll.i:n+ll.i-1]
+                                                             # `@views` handles Tuple
 @inline take_n(ll, n) = @views ll[1:n]
 
 drop_n(ll, n) = n === 0 ? ll : drop_n(cdr(ll), n-1)
 
 @inline drop_n(ll::Term, n) = drop_n(arguments(ll), n-1)
+
 @inline drop_n(ll::AbstractArray, n) = drop_n(LL(ll, 1), n)
+
 @inline drop_n(ll::LL, n) = LL(ll.v, ll.i+n)
 
+
+# Debugging
+
+const TIMER_OUTPUTS = true
+
+const being_timed = Ref{Bool}(false)
+
+if TIMER_OUTPUTS
+    using TimerOutputs
+
+    macro timer(name, expr)
+        :(if being_timed[]
+              @timeit $(esc(name)) $(esc(expr))
+          else
+              $(esc(expr))
+          end)
+    end
+
+    macro iftimer(expr)
+        esc(expr)
+    end
+else
+    macro timer(name, expr)
+        esc(expr)
+    end
+
+    macro iftimer(expr)
+    end
+end

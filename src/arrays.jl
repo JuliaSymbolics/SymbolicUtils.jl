553e6c25da785d0f5a54678575950f8f6fc1dc35
Shashi Gowda <shashigowda91@gmail.com>
2020-05-13 01:40:45 -0400

    maybe macro to deal with unknown info

--- src/arrays.jl
+++ src/arrays.jl
@@ -1,9 +1,13 @@
+const SymArray{T,N} = Symbolic{<:AbstractArray{T,N}}
+
 # Array interface, assumes that s.metadata is an ArrayShape, see below
 # TODO: if shape is not known these should return Symbolic results
 #
-Base.axes(s::Symbolic{<:AbstractArray}) = axes(s.metadata)
+Base.axes(s::SymArray) = axes(s.metadata)
 
-Base.size(s::Symbolic{<:AbstractArray}) = map(length, axes(s))
+Base.size(s::SymArray) = map(length, axes(s))
+
+Base.ndims(s::SymArray) = ndims(symtype(s))
 
 function Base.getindex(x::Symbolic{T}, idx::Int...) where {T<:AbstractArray}
     Term{eltype(T)}(getindex, idx...)
@@ -14,21 +18,48 @@ maybe(f, x) = x === nothing ? nothing : f(x)
 # allows promote_symtype to be called with the correct types
 symtype(x::Union{Colon, AbstractRange}) = typeof(x)
 
-function promote_symtype(::typeof(getindex),
-                         A::Type{AbstractArray{T,M}},
-                         idx...) where {T,M}
-    S = Core.Compiler.return_type(getindex,
-                                  Tuple{A, idx...})
-    if S != Union{} && (S <: AbstractArray || S <: eltype(A))
-        return S
-    end
-    N = count(x->x <: Number, idx)
-    N == M ? eltype(A) : AbstractArray{eltype(A), M-N}
+
+# Partial information
+elt(s::SymArray) = _eltype(symtype(s))
+elt(::Type{<:AbstractArray{T}}) where {T} = T
+elt(::Type{<:AbstractArray}) = nothing
+
+nd(s::SymArray) = _ndims(symtype(s))
+nd(::Type{<:AbstractArray{<:Any, N}}) where {N} = N
+nd(::Type{<:AbstractArray}) = nothing
+
+shape(s::SymArray) = metadata(s)
+
+macro maybe(args...)
+    f = args[end]
+    vars = args[1:end-1]
+    names = [(@assert v.head == :(=); v.args[1]) for v in vars]
+    quote
+        $(vars...)
+        if !any(isnothing, ($(names...),))
+            $f
+        end
+        # nothing otherwise
+    end |> esc
 end
 
-function Base.getindex(x::Symbolic{<:AbstractArray}, idx...)
-    shape = maybe(m -> m[idx...], metadata(x))
-    Term(getindex, shape, [x, idx...])
+function promote_symtype(::typeof(getindex),
+                         A::Type{<:AbstractArray},
+                         idx...)
+    lessdims = count(x->x <: Number, idx)
+    @maybe T=elt(A) begin
+        @maybe N=nd(A) return AbstractArray{T,N-lessdims}
+        return AbstractArray{T}
+    end
+
+    @maybe N=nd(A) return AbstractArray{T, N-lessdims} where T
+
+    return AbstractArray
+end
+
+function Base.getindex(x::SymArray, idx...)
+    shp = @maybe s=shape(x) s[idx...]
+    Term(getindex, shp, [x, idx...])
 end
 
 

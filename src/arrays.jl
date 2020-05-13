53a1e9202652c74ea41b10dc83240e5851002f70
Shashi Gowda <shashigowda91@gmail.com>
2020-05-13 00:09:45 -0400

    refactor to allow arrays of unknown size

--- src/arrays.jl
+++ src/arrays.jl
@@ -1,27 +1,50 @@
+# Array interface, assumes that s.metadata is an ArrayShape, see below
+# TODO: if shape is not known these should return Symbolic results
+#
+Base.axes(s::Symbolic{<:AbstractArray}) = axes(s.metadata)
+
+Base.size(s::Symbolic{<:AbstractArray}) = map(length, axes(s))
+
+function Base.getindex(x::Symbolic{T}, idx::Int...) where {T<:AbstractArray}
+    Term{eltype(T)}(getindex, idx...)
+end
+
+maybe(f, x) = x === nothing ? nothing : f(x)
+
+# allows promote_symtype to be called with the correct types
+symtype(x::Union{Colon, AbstractRange}) = typeof(x)
+
+function promote_symtype(::typeof(getindex), A, idx...)
+    S = Core.Compiler.return_type(getindex,
+                                  Tuple{A, idx...})
+    if S != Union{} && (S <: AbstractArray || S <: eltype(A))
+        return S
+    end
+    N = count(x->x <: Number, idx)
+    N == ndims(A) ? eltype(A) : AbstractArray{eltype(A), ndims(A)-N}
+end
+
+function Base.getindex(x::Symbolic{A}, idx...) where {A<:AbstractArray}
+
+    shape = maybe(m -> m[idx...], metadata(x))
+    Term(getindex, shape, [x, idx...])
+end
+
+
+# ArrayShape
+# Note: implement this as if it's an array
+# the idea is it needs to be usable both during construction
+# and simplification
 struct ArrayShape{T, N}
     axes::Tuple
 end
 
 Base.axes(a::ArrayShape) = a.axes
 
-Base.axes(s::Symbolic{<:AbstractArray}) = axes(s.metadata)
-
-function Base.getindex(x::Symbolic{T}, idx::Int...) where {T<:AbstractArray}
-    Term{eltype(T)}(getindex, idx...)
-end
-
-function Base.getindex(x::Symbolic{A}, idx...) where {T,M,A<:AbstractArray{T,M}}
-    axes = metadata(x).axes
+function Base.getindex(a::ArrayShape{T}, idx...) where {T}
+    axes = a.axes
     idx1 = to_indices(CartesianIndices(axes), axes, idx)
     newaxes = ([1:length(x) for x in idx1 if !(x isa Number)]...,)
     N = length(newaxes)
     newshape = ArrayShape{T, N}(newaxes)
-
-    S = Core.Compiler.return_type(getindex, Tuple{symtype(x), typeof.(idx1)...})
-    if S == Any || S == Union{}
-        S = AbstractArray{T, N}
-    end
-    Term{S}(getindex, newshape, [x, idx...])
 end
-
-
--- test/arrays.jl
+++ test/arrays.jl
@@ -3,7 +3,7 @@ using SymbolicUtils: metadata, symtype, ArrayShape
 using Base: Slice
 
 @testset "arrays" begin
-    @syms X[1:5, 2:6] (Y::Real)[1:5, 1:5]
+    @syms X[1:5, 2:6] (Y::Real)[1:5, 1:5] Z::Matrix{Float64}
     @test symtype(X) == AbstractArray{Number, 2}
     @test symtype(Y) == AbstractArray{Real, 2}
     @test metadata(X) == ArrayShape{Number, 2}(Slice.((1:5, 2:6)))
@@ -15,4 +15,6 @@ using Base: Slice
 
     B = A[3:5]
     @test axes(metadata(B)) == (Slice(1:3),)
+
+    @test metadata(Z[1:2, 3:4]) == nothing
 end

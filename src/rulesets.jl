6f24af5ed5e35ab5b74b4dd3948f7409156eaf68
Shashi Gowda <shashigowda91@gmail.com>
2020-05-19 13:43:52 -0400

    distribute power only for integers

--- src/rulesets.jl
+++ src/rulesets.jl
@@ -45,7 +45,7 @@ const TIMES_RULES = RuleSet([
 
 const POW_RULES = RuleSet([
     @rule(^(*(~~x), ~y) => *(map(a->pow(a, ~y), ~~x)...))
-    @rule((((~x)^(~p))^(~q)) => (~x)^((~p)*(~q)))
+    @rule((((~x)^(~p::isliteral(Integer)))^(~q::isliteral(Integer))) => (~x)^((~p)*(~q)))
     @rule(^(~x, ~z::_iszero) => 1)
     @rule(^(~x, ~z::_isone) => ~x)
 ])

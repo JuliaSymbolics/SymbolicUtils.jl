4c29aebca14f0399bfb5dee445422aa44090dbbc
Shashi Gowda <gowda@mit.edu>
2020-06-12 12:02:17 -0400

    export mpoly functionality separately as polynormalize

--- test/nf.jl
+++ test/nf.jl
@@ -1,8 +1,9 @@
 using SymbolicUtils, Test
+using SymbolicUtils: polynormalize
 @testset "polyform" begin
     @syms a b c d
-    @test simplify(a * (b + -1 * c) + -1 * (b * a + -1 * c * a), mpoly=true) == 0
-    @eqtest simplify(sin((a+b)^2)^2; mpoly=true) == sin(a^2+b^2+2*a*b)^2
+    @test polynormalize(a * (b + -1 * c) + -1 * (b * a + -1 * c * a), mpoly=true) == 0
+    @eqtest polynormalize(sin((a+b)^2)^2) == sin(a^2+b^2+2*a*b)^2
     # fixme: can this be made faster?
-    @test simplify(sin((a+b)^2)^2 + cos((a+b)^2)^2; mpoly=true) == 1
+    @test simplify(polynormalize(sin((a+b)^2)^2 + cos((a+b)^2)^2)) == 1
 end

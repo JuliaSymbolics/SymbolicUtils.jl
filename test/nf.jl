7086676b29e22f85cf9ea8b8e3fc4141bf6852a7
Shashi Gowda <shashigowda91@gmail.com>
2020-06-12 13:13:41 -0400

    Update test/nf.jl
    Co-authored-by: Yingbo Ma <mayingbo5@gmail.com>

--- test/nf.jl
+++ test/nf.jl
@@ -2,7 +2,7 @@ using SymbolicUtils, Test
 using SymbolicUtils: polynormalize
 @testset "polyform" begin
     @syms a b c d
-    @test polynormalize(a * (b + -1 * c) + -1 * (b * a + -1 * c * a), mpoly=true) == 0
+    @test polynormalize(a * (b + -1 * c) + -1 * (b * a + -1 * c * a)) == 0
     @eqtest polynormalize(sin((a+b)^2)^2) == sin(a^2+b^2+2*a*b)^2
     # fixme: can this be made faster?
     @test simplify(polynormalize(sin((a+b)^2)^2 + cos((a+b)^2)^2)) == 1

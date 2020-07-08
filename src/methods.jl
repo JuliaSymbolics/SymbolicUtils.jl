69da6a1e8a2a6526b6faf3091ab7f15ae1d3b179
Shashi Gowda <gowda@mit.edu>
2020-07-08 03:03:43 -0400

    eval_number_methods ono your own types

--- src/methods.jl
+++ src/methods.jl
@@ -2,25 +2,51 @@ const monadic = [deg2rad, rad2deg, transpose, -, conj, asind, log1p, acsch, acos
 
 const diadic = [+, -, max, min, *, /, \, hypot, atan, mod, rem, ^]
 
+const previously_declared_for = []
 # TODO: keep domains tighter than this
+function eval_number_methods(T, rhs1, rhs2)
+    exprs = []
+    for f in diadic
+        @show previously_declared_for
+        for S in vcat(previously_declared_for, T)
+            @show T, S
+            push!(exprs, quote
+                      @eval (::$(typeof(f)))(a::$T, b::$S) = term($f, a, b)
+                  end)
+        end
+
+        expr = quote
+            @eval begin
+                (f::$(typeof(f)))(a::$T, b::Real)   = $rhs2
+                (f::$(typeof(f)))(a::Real, b::$T)   = $rhs2
+                (f::$(typeof(f)))(a::$T, b::Number) = $rhs2
+                (f::$(typeof(f)))(a::Number, b::$T) = $rhs2
+            end
+        end
+
+        push!(exprs, expr)
+    end
+
+    for f in monadic
+        push!(exprs, :(@eval (f::$(typeof(f)))(a::$T)   = $rhs1))
+    end
+    push!(previously_declared_for, T)
+    Expr(:block, exprs...)
+end
+
+
+macro eval_number_methods(T, rhs1, rhs2)
+    eval_number_methods(T, rhs1, rhs2)
+end
+
+@eval_number_methods(Sym{<:Number}, term(f, a), term(f, a, b))
+@eval_number_methods(Term{<:Number}, term(f, a), term(f, a, b))
+
 for f in diadic
     @eval promote_symtype(::$(typeof(f)),
                    T::Type{<:Number},
                    S::Type{<:Number}) = promote_type(T, S)
-
-    for T in [Sym, Term]
-        for S in [Sym, Term]
-            @eval (::$(typeof(f)))(a::$T, b::$S) = term($f, a, b)
-        end
-        @eval begin
-            (::$(typeof(f)))(a::$T, b::Real)   = term($f, a, b)
-            (::$(typeof(f)))(a::Real, b::$T)   = term($f, a, b)
-            (::$(typeof(f)))(a::$T, b::Number) = term($f, a, b)
-            (::$(typeof(f)))(a::Number, b::$T) = term($f, a, b)
-        end
-    end
 end
-
 promote_symtype(::typeof(rem2pi), T::Type{<:Number}, mode) = T
 Base.rem2pi(x::Symbolic, mode::Base.RoundingMode) = term(rem2pi, x, mode)
 

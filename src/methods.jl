18f7ed67c7240f8364cd0951901e9be43abb9388
Shashi Gowda <gowda@mit.edu>
2021-01-15 19:05:21 -0500

    delete rec_promote_symtype, use foldl and fast paths, fix it on Sym{FnType}
    Co-authored-by: "Yingbo Ma" <mayingbo5@gmail.com>

--- src/methods.jl
+++ src/methods.jl
@@ -101,12 +101,6 @@ for f in monadic
     @eval (::$(typeof(f)))(a::Symbolic)   = term($f, a)
 end
 
-rec_promote_symtype(f) = promote_symtype(f)
-rec_promote_symtype(f, x) = promote_symtype(f, x)
-rec_promote_symtype(f, x,y) = promote_symtype(f, x,y)
-rec_promote_symtype(f, x,y,z...) = rec_promote_symtype(f, promote_symtype(f, x,y), z...)
-
-
 Base.:*(a::AbstractArray, b::Symbolic{<:Number}) = map(x->x*b, a)
 Base.:*(a::Symbolic{<:Number}, b::AbstractArray) = map(x->a*x, b)
 

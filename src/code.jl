f7b05fd6747fb6ef7786c4305c85c527840f5fef
Shashi Gowda <gowda@mit.edu>
2021-01-27 13:54:28 -0500

    wip

--- src/code.jl
+++ src/code.jl
@@ -2,6 +2,9 @@
 macro matchable(expr)
     @assert expr.head == :struct
     name = expr.args[2]
+    if name isa Expr && name.head === :curly
+        name = name.args[1]
+    end
     fields = expr.args[3].args  # Todo: get names
     quote
         $expr
@@ -11,7 +14,7 @@ macro matchable(expr)
     end |> esc
 end
 
-toexpr(s::Sym) = nameof(s)
+toexpr(s::Sym, st) = nameof(s)
 
 @matchable struct Assignment
     lhs
@@ -22,30 +25,42 @@ const (←) = Assignment
 
 Base.convert(::Type{Assignment}, p::Pair) = Assignment(pair[1], pair[2])
 
-toexpr(a::Assignment) = :($(toexpr(a.lhs)) = $(toexpr(b.lhs)))
+toexpr(a::Assignment, st) = :($(toexpr(a.lhs)) = $(toexpr(b.lhs)))
 
-function toexpr(O)
+struct NameState
+    tosym::Dict
+    sym_count::Dict
+end
+
+function toexpr(O, st)
     !istree(O) && return O
     op = operation(O)
     args = arguments(O)
     if op isa Differential
-        ex = toexpr(args[1])
-        wrt = toexpr(op.x)
+        ex = toexpr(args[1], st)
+        wrt = toexpr(op.x, st)
         return :(_derivative($ex, $wrt))
     elseif op isa Sym
         isempty(args) && return nameof(op)
-        return Expr(:call, toexpr(op), toexpr(args)...)
+        return Expr(:call, toexpr(op, st), toexpr(args, st)...)
     elseif op === (^) && length(args) == 2 && args[2] isa Number && args[2] < 0
-        ex = toexpr(args[1])
+        ex = toexpr(args[1], st)
         if args[2] == -1
-            return toexpr(Term{Any}(inv, ex))
+            return toexpr(Term{Any}(inv, ex), st)
         else
-            return toexpr(Term{Any}(^, [Term{Any}(inv, ex), -args[2]]))
+            return toexpr(Term{Any}(^, [Term{Any}(inv, ex), -args[2]]), st)
         end
     elseif op === (cond)
-        :($(toexpr(args[1])) ? $(toexpr(args[2])) : $(toexpr(args[3])))
+        :($(toexpr(args[1], st)) ? $(toexpr(args[2], st)) : $(toexpr(args[3], st)))
+    elseif op isa Sym # We can just use it as a variable
+        get!(st.tosym, O) do
+            c = get(st.sym_count, op, 0)
+            sym = c != 0 ? Symbol(op, :__, c) : op
+            st.tosym[O] = sym
+            st.sym_count[sym] = c + 1
+        end
     end
-    return Expr(:call, op, toexpr(args)...)
+    return Expr(:call, op, toexpr(args, st)...)
 end
 
 @matchable struct Let
@@ -99,6 +114,7 @@ end
 
 
 @matchable struct SetArray
+    inbounds::Bool
     arr::Sym
     elems  # Either iterator of Pairs or just an iterator
 end
@@ -113,11 +129,12 @@ function toexpr(a::AtIndex)
 end
 
 function toexpr(s::SetArray)
-    quote
-        $([:($(toexpr(s.arr))[$(ex isa AtIndex ? ex.i : i)]) = $(toexpr(ex))
+    ex = quote
+        $([:($(toexpr(s.arr))[$(ex isa AtIndex ? ex.i : i)] = $(toexpr(ex)))
            for (i, ex) in enumerate(s.elems)]...)
         nothing
     end
+    s.inbounds ? :(@inbounds begin $ex end) : ex
 end
 
 @matchable struct MakeArray{A<:AbstractArray} # Could be StaticArray
@@ -128,12 +145,21 @@ function toexpr(a::MakeArray)
     :([$(toexpr.(a.elems)...)])
 end
 
+using SparseArrays
+
 ## We use a separate type for Sparse Arrays to sidestep the need for
 ## iszero to be defined on the expression type
 @matchable struct MakeSparseArray
-    I
-    J
+    sparsity::SparseMatrixCSC
     V
+end
+
+function MakeSparseArray(I, J, V)
+end
+
+function toexpr(a::MakeArray)
+    sp = a.sparsity
+    :(SparseMatrixCSC(sp.m, sp.n, sp.colptr, sp.rowval, [$(toexpr.(a.elems)...)]))
 end
 
 @matchable struct MakeTuple

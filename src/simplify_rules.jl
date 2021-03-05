5f0aa81e1a2099ae1cdb1321601891279de1ee1b
Carsten Bauer <crstnbr@gmail.com>
2021-03-05 10:25:34 +0100

    Update src/simplify_rules.jl
    Co-authored-by: Mason Protter <mason.protter@icloud.com>

--- src/simplify_rules.jl
+++ src/simplify_rules.jl
@@ -82,7 +82,7 @@ let
         @acrule(sinh(~x)cosh(~y) + cosh(~x)sinh(~y) => sinh(~x + ~y))
         @acrule(cosh(~x)cosh(~y) + sinh(~x)sinh(~y) => cosh(~x + ~y))
         @acrule(sinh(~x)cosh(~y) + -1*cosh(~x)*sinh(~y) => sinh(~x - ~y))
-        @acrule(cosh(~x)cosh(~y) - sinh(~x)sinh(~y) => cosh(~x - ~y))
+        @acrule(cosh(~x)cosh(~y) + -1*sinh(~x)*sinh(~y) => cosh(~x - ~y))
         ]
 
     BOOLEAN_RULES = [

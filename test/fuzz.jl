eafc4376f074e2b76034ef3d1c8c6654d9260f0c
Yingbo Ma <mayingbo5@gmail.com>
2021-06-12 00:15:25 -0400

    Don't use testset in fuzz.jl
    It turns out that at testset macro sets the random seed

--- test/fuzz.jl
+++ test/fuzz.jl
@@ -3,35 +3,29 @@ include("fuzzlib.jl")
 using Random: seed!
 
 seed!(6174)
-@testset "Fuzz test" begin
-    @time @testset "expand fuzz" begin
-        for i=1:500
-            fuzz_test(5, num_spec, SymbolicUtils.expand; min_depth=3)
-        end
-    end
-    @time @testset "num fuzz" begin
-        for i=1:1500
-            fuzz_test(5, num_spec)
-        end
-    end
-    @time @testset "bool fuzz" begin
-        for i=1:500
-            seed!(i)
-            fuzz_test(5, bool_spec)
-        end
-    end
-    @testset "fuzz addmulpow" begin
-        @time for i=1:100
-            fuzz_addmulpow(1)
-        end
-        @time for i=1:50
-            fuzz_addmulpow(2)
-        end
-        @time for i=1:25
-            fuzz_addmulpow(3)
-        end
-        @time for i=1:12
-            fuzz_addmulpow(4)
-        end
-    end
+# expand fuzz
+@time for i=1:500
+    fuzz_test(5, num_spec, SymbolicUtils.expand; min_depth=3)
+end
+# num fuzz
+@time for i=1:1500
+    fuzz_test(5, num_spec)
+end
+# bool fuzz
+@time for i=1:500
+    seed!(i)
+    fuzz_test(5, bool_spec)
+end
+# fuzz addmulpow
+@time for i=1:100
+    fuzz_addmulpow(1)
+end
+@time for i=1:50
+    fuzz_addmulpow(2)
+end
+@time for i=1:25
+    fuzz_addmulpow(3)
+end
+@time for i=1:12
+    fuzz_addmulpow(4)
 end

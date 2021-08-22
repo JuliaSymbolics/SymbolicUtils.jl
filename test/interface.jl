dd9552719cdc79f93d2fc05ab719bba2a3a02bd3
Alessandro <none>
2021-08-22 14:09:43 +0200

    interface tests fixed

--- test/interface.jl
+++ test/interface.jl
@@ -4,7 +4,8 @@ using TermInterface
 
 TermInterface.isterm(ex::Expr) = ex.head == :call
 TermInterface.gethead(ex::Expr) = ex.args[1]
-SymbolicUtils.getargs(ex::Expr) = ex.args[2:end]
+TermInterface.getargs(ex::Expr) = ex.args[2:end]
+TermInterface.similarterm(x::Type{Expr}, head, args; type=nothing, metadata=nothing) = Expr(:call, head, args...)
 
 for f ∈ [:+, :-, :*, :/, :^]
     @eval begin
@@ -14,11 +15,11 @@ for f ∈ [:+, :-, :*, :/, :^]
     end
 end
 
-ex = 1 + (:x - 2)
-
-@test simplify(ex) == ex
-
+Base.zero(t::Expr) = 0
 TermInterface.symtype(::Expr) = Real
 TermInterface.symtype(::Symbol) = Real
+
+ex = 1 + (:x - 2)
+
 @test simplify(ex) == -1 + :x
 @test simplify(:a * (:b + -1 * :c) + -1 * (:b * :a + -1 * :c * :a), expand=true) == 0

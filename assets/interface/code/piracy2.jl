aa230423e8b3a7e8d2ab03c7fdf09c50c65ee1fb
shashi <gowda@mit.edu>
2021-08-30 17:37:16 +0000

    Deploying to gh-pages from  @ e3189c6ac43e726ff8378e2dd878e7936c0c7190 🚀

--- assets/interface/code/piracy2.jl
+++
@@ -1,8 +0,0 @@
-# This file was generated, do not modify it. # hide
-using SymbolicUtils
-
-SymbolicUtils.istree(ex::Expr) = ex.head == :call
-SymbolicUtils.operation(ex::Expr) = ex.args[1]
-SymbolicUtils.arguments(ex::Expr) = ex.args[2:end]
-
-@rule(~x => ~x - 1)(ex)
--- assets/interface/code/piracy4.jl
+++
@@ -1,4 +0,0 @@
-# This file was generated, do not modify it. # hide
-SymbolicUtils.symtype(s::Expr) = Number
-
-simplify(ex)

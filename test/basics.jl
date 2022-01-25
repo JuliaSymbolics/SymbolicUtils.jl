f0f9b47297a320bf3587ebfa8248c339b2a1c02e
jtrakk <43392409+jtrakk@users.noreply.github.com>
2022-01-24 23:50:30 -0800

    Remove fold in test

--- test/basics.jl
+++ test/basics.jl
@@ -89,9 +89,9 @@ struct Ctx2 end
     @test getmetadata(newf, Ctx1) == "yes"
 
 
-    @test isequal(substitute(1+sqrt(a), Dict(a => 2), fold=false),
+    @test isequal(substitute(1+sqrt(a), Dict(a => 2)),
                   1 + term(sqrt, 2, type=Number))
-    @test substitute(1+sqrt(a), Dict(a => 2), fold=true) isa Float64
+    @test substitute(1+sqrt(a), Dict(a => 2)) isa Float64
 end
 
 @testset "Base methods" begin

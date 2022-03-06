7e29bdf0f1c2d4552828490eaf75eb8963c008c6
Alessandro <none>
2022-03-06 14:52:50 +0100

    istree dispatch on object instead of type

--- src/polyform.jl
+++ src/polyform.jl
@@ -168,7 +168,7 @@ function PolyForm(x,
     PolyForm{symtype(x)}(p, pvar2sym, sym2term, metadata)
 end
 
-TermInterface.istree(x::Type{<:PolyForm}) = true
+TermInterface.istree(x::PolyForm) = true
 
 TermInterface.operation(x::PolyForm) = MP.nterms(x.p) == 1 ? (*) : (+)
 
--- test/egraph.jl
+++ test/egraph.jl
@@ -5,16 +5,16 @@ sym_eq(x, y) = simplify(x == y)
 
 @testset "Basic optimization" begin
     @syms a b c x y z
-    ex = 2a + 2b - (a*(a + b))
+    ex = 2a + 2b - (a * (a + b))
     res = optimize(ex)
-    @test_broken sym_eq(res, (a+b)*(2-a))
+    @test sym_eq(res, (a + b) * (2 - a))
 
-    ex = sin(a^2)/cos(a^2)
+    ex = sin(a^2) / cos(a^2)
     res = optimize(ex)
-    @test_broken isequal(res, tan(a^2)) # sym_eq does not work
+    @test isequal(res, tan(a^2)) # sym_eq does not work
     # TODO report issue
 
-    ex = sin(1/x * (a * b + a * c))^2 + cos((a*(b+c))/x)^2
+    ex = sin(1 / x * (a * b + a * c))^2 + cos((a * (b + c)) / x)^2
     res = optimize(ex)
-    @test_broken res == 1
+    @test res == 1
 end

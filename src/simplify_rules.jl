99d5321ba6c01c0887c0144647e7ee4732ed97e9
Ashley Milsted <ashmilsted@gmail.com>
2023-06-29 17:28:23 -0700

    Add real + imag

--- src/simplify_rules.jl
+++ src/simplify_rules.jl
@@ -68,6 +68,7 @@ let
         @rule(imag(~x::_isreal) => zero(symtype(~x)))
         @rule(~x + conj(~x) => 2 * real(~x))
         @rule(~x - conj(~x) => 2im * imag(~x))
+        @rule(real(~x) + imag(~x) => x)
         @rule(ifelse(~x::is_literal_number, ~y, ~z) => ~x ? ~y : ~z)
         @rule(ifelse(~x, ~y, ~y) => ~y)
     ]

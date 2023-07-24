ee0ea9824c95f10f6b6ed97af568963bdb681bb3
Ashley Milsted <ashmilsted@gmail.com>
2023-07-24 11:38:40 -0700

    Fix typos.

--- src/simplify_rules.jl
+++ src/simplify_rules.jl
@@ -68,8 +68,8 @@ let
         @rule(imag(~x::_isreal) => zero(symtype(~x)))
         @rule(~x + conj(~x) => 2 * real(~x))
         @rule(~x - conj(~x) => 2im * imag(~x))
-        @rule(real(~x) + imag(~x) => x)
-        @rule(imag(~x) + real(~x) => x)
+        @rule(real(~x) + imag(~x) => ~x)
+        @rule(imag(~x) + real(~x) => ~x)
         @rule(ifelse(~x::is_literal_number, ~y, ~z) => ~x ? ~y : ~z)
         @rule(ifelse(~x, ~y, ~y) => ~y)
     ]

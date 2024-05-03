07fdc5f6c1bfc594a2f58785535a7cc76f51b531
Sean P. Cornelius <spcornelius@gmail.com>
2024-05-03 12:45:46 -0400

    add test for nested CSE

--- test/cse.jl
+++ test/cse.jl
@@ -1,10 +1,16 @@
 using SymbolicUtils, SymbolicUtils.Code, Test
 @testset "CSE" begin
-    @syms x
+    @syms x y
     t = cse(hypot(hypot(cos(x), sin(x)), atan(cos(x), sin(x))))
 
     @test t isa Let
     @test length(t.pairs) == 2
     @test occursin(t.pairs[1].lhs, t.body)
     @test occursin(t.pairs[2].lhs, t.body)
+
+    t = cse((x*y) + sin(x*y) + sqrt(sin(x*y)))
+
+    @test t isa Let
+    @test length(t.pairs) == 2
+    @test !occursin(x*y, t.pairs[2].rhs)
 end

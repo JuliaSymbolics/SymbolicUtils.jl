4e459bbbd8db1e60d3d44f296ec62b0c36f0c2e0
Bowen S. Zhu <bowenzhu@mit.edu>
2024-06-28 12:39:53 -0700

    Modify `(f::Symbolic{<:FnType})(args...)`

--- src/types.jl
+++ src/types.jl
@@ -912,7 +912,7 @@ promote_symtype(f, Ts...) = Any
 
 struct FnType{X<:Tuple,Y} end
 
-(f::Symbolic{<:FnType})(args...) = Term{promote_symtype(f, symtype.(args)...)}(f, Any[args...])
+(f::Symbolic{<:FnType})(args...) = _Term(promote_symtype(f, symtype.(args)...), f, [args...])
 
 function (f::Symbolic)(args...)
     error("Sym $f is not callable. " *

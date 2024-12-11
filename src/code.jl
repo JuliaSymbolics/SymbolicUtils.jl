4c16785f43c68cd7894a3a92e87f0fccd5daf2da
benedict-96 <benedikt.brantner@icloud.com>
2024-12-11 10:05:23 +0100

    Implemented method 'create_array' for Base.ReshapedArrays.

--- src/code.jl
+++ src/code.jl
@@ -560,6 +560,10 @@ end
     create_array(P, S, nd, d, elems...)
 end
 
+@inline function create_array(::Type{<:Base.ReshapedArray{T, N, P}}, S, nd::Val, d, elems...) where {T, N, P}
+    create_array(P, S, nd, d, elems...)
+end
+
 ## SArray
 @inline function create_array(::Type{<:SArray}, ::Nothing, nd::Val, ::Val{dims}, elems...) where dims
     SArray{Tuple{dims...}}(elems...)

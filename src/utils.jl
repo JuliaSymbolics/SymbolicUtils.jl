6ab4078a29b2cb1525df23acf0ce9df39de08b11
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-08-04 10:33:25 -0400

    fix: handle `Const` in `_iszero`, `_isone`

--- src/utils.jl
+++ src/utils.jl
@@ -42,13 +42,13 @@ is_literal_number(x) = isliteral(Number)(x)
 
 # checking the type directly is faster than dynamic dispatch in type unstable code
 function _iszero(x)
-    x = unwrap(x)
+    x = unwrap_const(unwrap(x))
     x isa Number && return iszero(x)
     x isa Array && return iszero(x)
     return false
 end
 function _isone(x)
-    x = unwrap(x)
+    x = unwrap_const(unwrap(x))
     x isa Number && return isone(x)
     x isa Array && return isone(x)
     return false

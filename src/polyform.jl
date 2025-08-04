a1e81320f6dc850f4115d96516229a79e27599fa
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-08-04 10:38:09 -0400

    fix: fix `add_with_div`

--- src/polyform.jl
+++ src/polyform.jl
@@ -278,7 +278,8 @@ function add_with_div(x)
         isdiv(a) || continue
         push!(dens, a.den)
     end
-    den = mul_worker(dens)
+    T = symtype(x)
+    den = mul_worker(T, dens)
 
     # add all numerators
     div_idx = 1
@@ -291,7 +292,7 @@ function add_with_div(x)
         if isdiv(a)
             _den = dens[div_idx]
             dens[div_idx] = a.num
-            _num = mul_worker(dens)
+            _num = mul_worker(T, dens)
             dens[div_idx] = _den
             div_idx += 1
         else

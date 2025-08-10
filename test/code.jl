b91b2c84aa573ef47fab6078dffb605692365261
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-08-10 16:38:45 -0400

    test: update codegen tests to new variant structure

--- test/code.jl
+++ test/code.jl
@@ -21,10 +21,10 @@ nanmath_st.rewrites[:nanmath] = true
     @test toexpr(a+b+c+d+e) == :($(+)($(+)($(+)($(+)(a, b), c), d), e))
     @test toexpr(a+b) == :($(+)(a, b))
     @test toexpr(x(t)+y(t)) == :($(+)(y(t), x(t)))
-    @test toexpr(x(t)+y(t)+x(t+1)) == :($(+)($(+)(x($(+)(1, t)), y(t)), x(t)))
+    @test toexpr(x(t)+y(t)+x(t+1)) == :($(+)($(+)(y(t), x($(+)(1, t))), x(t)))
     s = LazyState()
     Code.union_rewrites!(s.rewrites, [x(t), y(t)])
-    @test toexpr(x(t)+y(t)+x(t+1), s) == :($(+)($(+)(x($(+)(1, t)), var"y(t)"), var"x(t)"))
+    @test toexpr(x(t)+y(t)+x(t+1), s) == :($(+)($(+)(var"y(t)", x($(+)(1, t))), var"x(t)"))
 
     ex = :(let a = 3, b = $(+)(1,a)
                $(+)(a, b)
@@ -38,7 +38,7 @@ nanmath_st.rewrites[:nanmath] = true
 
     test_repr(toexpr(Func([x(t), x],[b ← a+2, y(t) ← b], x(t)+x(t+1)+b+y(t))),
               :(function (var"x(t)", x; b = $(+)(2, a), var"y(t)" = b)
-                    $(+)($(+)($(+)(b, x($(+)(1, t))), var"y(t)"), var"x(t)")
+                    $(+)($(+)($(+)(b, var"y(t)"), x($(+)(1, t))), var"x(t)")
                 end))
     test_repr(toexpr(Func([DestructuredArgs([x, x(t)], :state),
                            DestructuredArgs((a, b), :params)], [],
